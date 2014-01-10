07c12882b91b3daf4011351c06b522229227e387
Jerzy Kozera <jerzy.kozera@gmail.com>
2014-01-10 18:20:09 +0100

    Workaround for AngularJS docset and Windows

--- zeal/zealnetworkaccessmanager.cpp
+++ zeal/zealnetworkaccessmanager.cpp
@@ -17,5 +17,22 @@ QNetworkReply * ZealNetworkAccessManager::createRequest(QNetworkAccessManager::O
         return QNetworkAccessManager::createRequest(QNetworkAccessManager::GetOperation,
                                                     QNetworkRequest(QUrl()));
     }
+#ifdef WIN32
+    // Fix for AngularJS docset - Windows doesn't allow ':'s in filenames,
+    // and bsdtar.exe replaces them with '_'s, so replace all ':'s in requests
+    // with '_'s.
+    QNetworkRequest winReq(req);
+    QUrl winUrl(req.url());
+    QString winPath = winUrl.path();
+    // absolute paths are of form /C:/..., so don't replace colons occuring
+    // within first 3 characters
+    while(winPath.lastIndexOf(':') > 2) {
+        winPath = winPath.replace(winPath.lastIndexOf(':'), 1, "_");
+    }
+    winUrl.setPath(winPath);
+    winReq.setUrl(winUrl);
+    return QNetworkAccessManager::createRequest(op, winReq, outgoingData);
+#else
     return QNetworkAccessManager::createRequest(op, req, outgoingData);
+#endif
 }
