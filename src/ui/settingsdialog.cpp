zealdocs/zeal

commit f3cedc31af99d0cc6fb994b8ea24ba1998c97699

SettingsDialog: Properly abort downloads not form the docset list
cancelDownloads() must cancel all downloads, not only docset archives.

src/ui/settingsdialog.cpp
@@ -573,9 +573,8 @@ void SettingsDialog::cancelDownloads()
         QListWidgetItem *listItem
                 = ui->availableDocsetList->item(reply->property(ListItemIndexProperty).toInt());
         if (!listItem)
-            continue;
+            listItem->setData(ProgressItemDelegate::ShowProgressRole, false);
 
-        listItem->setData(ProgressItemDelegate::ShowProgressRole, false);
         reply->abort();
     }
     resetProgress();

commit b0d48ea1404df185972c4966afe661601ab38bc4

SettingsDialog: Fix download progress not hiding on cancel
Regression introduced in f3cedc31af99d0cc6fb994b8ea24ba1998c97699.

src/ui/settingsdialog.cpp
@@ -590,7 +590,7 @@ void SettingsDialog::cancelDownloads()
         // Hide progress bar
         QListWidgetItem *listItem
                 = ui->availableDocsetList->item(reply->property(ListItemIndexProperty).toInt());
-        if (!listItem)
+        if (listItem)
             listItem->setData(ProgressItemDelegate::ShowProgressRole, false);
 
         if (reply->property(DownloadTypeProperty).toInt() == DownloadDocset)