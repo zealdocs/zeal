5fe6480bd70d638462e4a0d7833392bb33d14e57
Oleg Shparber <oleg@zealdocs.org>
2016-03-27 10:37:48 -0400

    core: Fix character corruption in docset extraction (fixes #508)
    The problem is caused by lack of UTF-8 support in libarchive, and
    affects non-Latin Windows (e.g. Chinese).
    
    This change is rather a workaround, than a fix.

--- src/core/extractor.cpp
+++ src/core/extractor.cpp
@@ -63,7 +63,12 @@ void Extractor::extract(const QString &filePath, const QString &destination, con
     /// TODO: Do not strip root directory in archive if it equals to 'root'
     archive_entry *entry;
     while (archive_read_next_header(info.archiveHandle, &entry) == ARCHIVE_OK) {
+#ifndef Q_OS_WIN32
         QString pathname = QString::fromUtf8(archive_entry_pathname(entry));
+#else
+        /// TODO: Remove once https://github.com/libarchive/libarchive/issues/587 is resolved.
+        QString pathname = QString::fromWCharArray(archive_entry_pathname_w(entry));
+#endif
         if (!root.isEmpty())
             pathname.remove(0, pathname.indexOf(QLatin1String("/")) + 1);
         archive_entry_set_pathname(entry, qPrintable(destinationDir.absoluteFilePath(pathname)));
