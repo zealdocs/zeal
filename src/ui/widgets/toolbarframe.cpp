f45ceea365fa87a62408628942f02b273ae946b1
Stephen Pegoraro <spegoraro@tutive.com>
2016-08-09 18:37:15 +0800

    Change line color at the bottom of ToolBarFrame

--- src/ui/widgets/toolbarframe.cpp
+++ src/ui/widgets/toolbarframe.cpp
@@ -13,7 +13,7 @@ void ToolBarFrame::paintEvent(QPaintEvent *event)
     // Draw a line at the bottom.
     QPainter painter(this);
     painter.save();
-    painter.setPen(palette().dark().color());
+    painter.setPen(palette().mid().color());
     painter.drawLine(0, height() - 1, width(), height() - 1);
     painter.restore();
 }
