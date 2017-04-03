bb89f299773b180f36185c155f6fa3d2639f635a
Oleg Shparber <oleg@zealdocs.org>
2017-04-03 00:36:44 -0400

    ui: Use lighter border color in ToolBarFrame

--- src/libs/ui/widgets/toolbarframe.cpp
+++ src/libs/ui/widgets/toolbarframe.cpp
@@ -16,8 +16,6 @@ void ToolBarFrame::paintEvent(QPaintEvent *event)
 
     // Draw a line at the bottom.
     QPainter painter(this);
-    painter.save();
-    painter.setPen(palette().dark().color());
-    painter.drawLine(0, height() - 1, width(), height() - 1);
-    painter.restore();
+    painter.setPen(palette().mid().color());
+    painter.drawLine(0, height() - 1, width() - 1, height() - 1);
 }
