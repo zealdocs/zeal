82436dd55d28d53a3a7cd0efeb9e7c958ae41462
Karthik Nishanth <nishanthkarthik@live.com>
2018-10-06 18:20:57 +0530

    Enable JavaScript only when Info.plist specifies isJavaScriptEnabled

--- src/libs/ui/widgets/webviewtab.h
+++ src/libs/ui/widgets/webviewtab.h
@@ -52,6 +52,7 @@ public:
 
     int zoomLevel() const;
     void setZoomLevel(int level);
+    void setJavaScriptEnabled(bool isJavaScriptEnabled);
 
     void setWebBridgeObject(const QString &name, QObject *object);
 
