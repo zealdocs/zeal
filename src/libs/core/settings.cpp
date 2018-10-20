zealdocs/zeal

commit b7d7e0a6a9b889228218110bb2330a303b773c62

fix(core): use relative storage path in portable build by default

src/libs/core/settings.cpp
@@ -158,7 +158,7 @@ void Settings::load()
         docsetPath = QStandardPaths::writableLocation(QStandardPaths::DataLocation)
                 + QLatin1String("/docsets");
 #else
-        docsetPath = QCoreApplication::applicationDirPath() + QLatin1String("/docsets");
+        docsetPath = QStringLiteral("docsets");
 #endif
         QDir().mkpath(docsetPath);
     }

commit d34d7b50e034b37c9cdb138bc6474efc24645fd6

fix(core): fix docset storage not created in portable build
Regression from b7d7e0a6a9b889228218110bb2330a303b773c62.

src/libs/core/settings.cpp
@@ -26,6 +26,7 @@
 
 #include <QCoreApplication>
 #include <QDir>
+#include <QFileInfo>
 #include <QSettings>
 #include <QStandardPaths>
 #include <QUrl>
@@ -160,9 +161,19 @@ void Settings::load()
 #else
         docsetPath = QStringLiteral("docsets");
 #endif
-        QDir().mkpath(docsetPath);
     }
     settings->endGroup();
+
+    // Create the docset storage directory if it doesn't exist.
+    const QFileInfo fi(docsetPath);
+    if (!fi.exists()) {
+        // TODO: Report QDir::mkpath() errors.
+        if (fi.isRelative()) {
+            QDir().mkpath(QCoreApplication::applicationDirPath() + "/" + docsetPath);
+        } else {
+            QDir().mkpath(docsetPath);
+        }
+    }
 
     settings->beginGroup(GroupState);
     windowGeometry = settings->value(QStringLiteral("window_geometry")).toByteArray();