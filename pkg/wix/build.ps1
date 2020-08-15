7b5fcbe4c254dd7d377b0afd52634b07db75045f
Oleg Shparber <oleg@zealdocs.org>
2020-08-15 15:27:00 -0400

    build(wix): add dev build mode

--- pkg/wix/build.ps1
+++ pkg/wix/build.ps1
@@ -1,5 +1,4 @@
 # TODO: This script should be replaced with wixproj and/or CPack.
-# TODO: Add Debug mode to override CompressionLevel with "none".
 
 # Sample command:
 # .\build.ps1 -Arch x64 -Version 0.4.0 -PackagePath "c:\tmp\zeal-0.4.0-windows-x64" -SignMsi
@@ -16,6 +15,7 @@ param(
     [Parameter(Mandatory=$True)]
     [string]$PackagePath,
 
+    [Switch]$DevBuild,
     [Switch]$SignMsi
 )
 
@@ -32,8 +32,12 @@ function CleanUp {
 
 Write-Output "Building $MsiFilename..."
 
+if ($DevBuild) {
+    $compressionLevelArg = '-dCompressionLevel="none"'
+}
+
 Write-Output "Running candle..."
-& candle.exe -nologo -pedantic -wx -arch "$Arch" -dAppVersion="$Version" -dAppPackageDir="$PackagePath" -o "$WixobjFilename" zeal.wxs
+& candle.exe -nologo -pedantic -wx -arch "$Arch" -dAppVersion="$Version" -dAppPackageDir="$PackagePath" $compressionLevelArg -o "$WixobjFilename" zeal.wxs
 if ($LastExitCode -ne 0) {
     CleanUp
     throw "candle failed with exit code $LastExitCode."
