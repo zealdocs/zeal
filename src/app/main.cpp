d4d7d35c3e137804d583c1b33c2c0116d796fbb6
Oleg Shparber <oleg@zealdocs.org>
2020-09-20 00:47:02 -0400

    fix(assets): update AppStream ID to org.zealdocs.zeal
    AppStream specification strongly encourages lowercase ID.
    Flatpak documentation recommends to avoid .desktop suffix in the ID.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -216,7 +216,7 @@ int main(int argc, char *argv[])
     }
 
     // Set application-wide window icon. All message boxes and other windows will use it by default.
-    qapp->setDesktopFileName(QStringLiteral("org.zealdocs.Zeal.desktop"));
+    qapp->setDesktopFileName(QStringLiteral("org.zealdocs.zeal.desktop"));
     qapp->setWindowIcon(QIcon::fromTheme(QStringLiteral("zeal"),
                                          QIcon(QStringLiteral(":/zeal.ico"))));
 
